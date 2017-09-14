1fb3ffc4a5b33a1bfea0bdf721bc77fe441d2805
Xing Li <msuli_1120@ymail.com>
2017-09-13 23:50:59 -0700

    add registration form

---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,45 @@
+<?php
+	namespace AppBundle\Controller;
+
+	use AppBundle\Entity\Member;
+	use AppBundle\Form\Type\MemberType;
+	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\HttpFoundation\Request;
+
+	class RegistrationController extends Controller
+	{
+		/**
+		 * @Route("/register", name="registration")
+		 * @return \Symfony\Component\HttpFoundation\Response
+		 * @throws \LogicException
+		 */
+		public function registerAction(Request $request) {
+			$member = new Member();
+			$form = $this->createForm(MemberType::class, $member, [
+			]);
+
+			$form->handleRequest($request);
+			if ($form->isSubmitted() && $form->isValid()){
+				$password = $this
+					->get('security.password_encoder')
+					->encodePassword(
+						$member,
+						$member->getPlainPassword()
+					)
+				;
+				$member->setPassword( $password);
+
+				$em = $this->getDoctrine()->getManager();
+				$em->persist( $member);
+				$em->flush();
+
+				$this->addFlash( 'success', 'You are now successfully registered');
+				$this->redirectToRoute( 'homepage');
+			}
+
+			return $this->render('registration/register.html.twig', [
+				'registration_form' => $form->createView(),
+			]);
+		}
+	}
